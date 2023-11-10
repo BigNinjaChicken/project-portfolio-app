86ffc0b67e045bb7b48a8b1946fd36c4343f6532
BigNinjaChicken <50557956+BigNinjaChicken@users.noreply.github.com>
2023-11-10 07:28:14 -0500

    Update +layout.svelte

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -90,7 +90,7 @@
 						href="/portfolio"
 						class="inline-flex items-center justify-center p-3 text-base font-medium text-gray-500 rounded-lg bg-gray-50 hover:text-gray-900 hover:bg-gray-100 dark:text-gray-400 dark:bg-gray-800 dark:hover:bg-gray-700 dark:hover:text-white"
 					>
-						<a href="/portfolio"><span class="w-full">🚀 All Programming Projects</span></a>
+						<span class="w-full">🚀 All Programming Projects</span>
 						<svg
 							class="w-2 h-2 ml-1"
 							aria-hidden="true"
@@ -140,7 +140,7 @@
 				href="./portfolio"
 				class="text-gray-300 hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium"
 			>
-				<a href="./"><span class="w-full">🚀 My Programming Projects</span></a>
+				<span class="w-full">🚀 My Programming Projects</span>
 			</a>
 		</div>
 	</div>

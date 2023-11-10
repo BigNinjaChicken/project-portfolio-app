848f9379ab42f364b8506bf09189d86cabe32ebc
BigNinjaChicken <50557956+BigNinjaChicken@users.noreply.github.com>
2023-11-10 00:13:44 -0500

    add main page

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,74 @@
+<script>
+	import '../app.postcss';
+	import "../app.css";
+</script>
+
+<nav class="bg-gray-800">
+	<div class="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
+	  <div class="relative flex h-16 items-center justify-between">
+		<div class="absolute inset-y-0 left-0 flex items-center sm:hidden">
+		  <!-- Mobile menu button-->
+		  <button type="button" class="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white" aria-controls="mobile-menu" aria-expanded="false">
+			<span class="absolute -inset-0.5"></span>
+			<span class="sr-only">Open main menu</span>
+			<!--
+			  Icon when menu is closed.
+  
+			  Menu open: "hidden", Menu closed: "block"
+			-->
+			<svg class="block h-6 w-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
+			  <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+			</svg>
+			<!--
+			  Icon when menu is open.
+  
+			  Menu open: "block", Menu closed: "hidden"
+			-->
+			<svg class="hidden h-6 w-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
+			  <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
+			</svg>
+		  </button>
+		</div>
+		<div class="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
+		  <div class="flex flex-shrink-0 items-center">
+			<img class="h-8 w-autotransition-all duration-300 cursor-pointer filter grayscale hover:grayscale-0" src="frazier-logo-purple.png" alt="Frazier Kyle">
+		  </div>
+		  <div class="hidden sm:ml-4 sm:block">
+			<div class="flex space-x-4">
+			  <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
+			  <a href="./" class="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">Dashboard</a>
+			  <a href="./" class="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">About Me</a>
+			  <a href="./" class="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">Full Stack Dev</a>
+			</div>
+		  </div>
+		</div>
+		<div class="hidden sm:ml-4 sm:block">
+			<div class="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
+				<a href="./" class="inline-flex items-center justify-center p-3 text-base font-medium text-gray-500 rounded-lg bg-gray-50 hover:text-gray-900 hover:bg-gray-100 dark:text-gray-400 dark:bg-gray-800 dark:hover:bg-gray-700 dark:hover:text-white">
+					<span class="w-full">🚀 My Programming Projects</span>
+					<svg class="w-2 h-2 ml-1" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+						<path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9" />
+					</svg>
+				</a>
+			</div>	
+		</div>	
+	  </div>
+	</div>
+  
+	<!-- Mobile menu, show/hide based on menu state. -->
+	<div class="sm:hidden" id="mobile-menu">
+	  <div class="space-y-1 px-2 pb-3 pt-2">
+		<!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
+		<a href="./" class="text-gray-300 hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium">Dashboard</a>
+		<a href="./" class="text-gray-300 hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium">About Me</a>
+		<a href="./" class="text-gray-300 hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium">Full Stack Dev</a>
+		<a href="./" class="inline-flex items-center justify-center p-3 text-base font-medium text-gray-500 rounded-lg bg-gray-50 hover:text-gray-900 hover:bg-gray-100 dark:text-gray-400 dark:bg-gray-800 dark:hover:bg-gray-700 dark:hover:text-white">
+			<span class="w-full">🚀 My Programming Projects</span>
+			<svg class="w-2 h-2 ml-1" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+				<path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9" />
+			</svg>
+		</a>
+	  </div>
+	</div>
+  </nav>
+<slot />
